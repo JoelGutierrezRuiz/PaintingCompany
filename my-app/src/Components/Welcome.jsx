4c1169abdd1dfee37702a11cef976aae67fa348c
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-11-22 18:04:18 -0800

    buscando colores naranjas

--- my-app/src/Components/Welcome.jsx
+++ my-app/src/Components/Welcome.jsx
@@ -1,13 +1,14 @@
 //imports 
 import React from "react";
 import '../Styles/Welcome.css'
-import painter from '../Imagenes/painter.jpg'
+import painter from '../Imagenes/painter.jpeg'
 //imports
 function Welcome (){
     return(
         <div className="mainContainer__welcome">
             <div className="welcome-text">HOUSE PAINTING SERVICES</div>
-            <img className="welcome-img" src={painter}></img>
+            <div className="welcome-img-container"><img className="welcome-img" src={painter}></img></div>
+            
         </div>
     );
 }
