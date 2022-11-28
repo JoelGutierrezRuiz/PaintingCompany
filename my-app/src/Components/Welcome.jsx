7802ca9e9dfda7855269f0e6ec86b6b43721a8ab
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-11-27 20:41:03 -0800

    El welcome ya tiene un texto de presentacion

--- my-app/src/Components/Welcome.jsx
+++ my-app/src/Components/Welcome.jsx
@@ -7,6 +7,10 @@ import Experience from "./Experience";
 function Welcome (){
     return(
         <div className="mainContainer__welcome">
+            <div className="welcome-container"> 
+                <p className="welcome-text"> Painting your safe space and family confort Since 2004 </p>
+                <p className="welcome-text-mini">Over more than a half a century, we’ve come to be Southern California’s center of knowledge, sales & service for fireplaces, fire pits & chimneys</p>
+            </div>
             <Experience></Experience>
             <img className="welcome-img" src={painter}></img>
            
