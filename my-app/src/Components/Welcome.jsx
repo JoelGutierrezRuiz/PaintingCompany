10b48e2822b5193f5137b2101bd5e9cec0c102ce
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-02 12:20:03 -0800

    Telefono implementado

--- my-app/src/Components/Welcome.jsx
+++ my-app/src/Components/Welcome.jsx
@@ -1,13 +1,14 @@
 //imports 
 import React from "react";
 import '../Styles/Welcome.css'
-//import painter from '../Imagenes/painter.jpeg'
+import { BsFillTelephoneFill } from 'react-icons/bs';
 import Experience from "./Experience";
 //imports
 function Welcome (){
     return(
         <div className="mainContainer__welcome">
-            <a className="welcome-phone" href="tel:5554280940">Call us at 555-428-0940</a>
+
+            <div className="welcome-phone-container"><BsFillTelephoneFill onClick={()=>{window.open("tel:+4842566");  }} className="welcome-phone-icon"></BsFillTelephoneFill><p className="welcome-phone-text" onClick={()=>{window.open("tel:+4842566");  }}>Call Us</p></div>
             <Experience></Experience>
             <img className="welcome-img" src={"https://www.marsanmantenimiento.com/wp-content/uploads/2020/10/men-painting-empty-home-interior-P96RH5M-1536x1024.jpg"}></img>
            
