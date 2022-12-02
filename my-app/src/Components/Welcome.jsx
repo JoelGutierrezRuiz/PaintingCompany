00c9233de3aa70e3522879bad1cb5413513aaa9a
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-02 11:53:27 -0800

    telefono prueba

--- my-app/src/Components/Welcome.jsx
+++ my-app/src/Components/Welcome.jsx
@@ -7,7 +7,7 @@ import Experience from "./Experience";
 function Welcome (){
     return(
         <div className="mainContainer__welcome">
-
+            <a className="welcome-phone" href="tel:5554280940">Call us at 555-428-0940</a>
             <Experience></Experience>
             <img className="welcome-img" src={"https://www.marsanmantenimiento.com/wp-content/uploads/2020/10/men-painting-empty-home-interior-P96RH5M-1536x1024.jpg"}></img>
            
