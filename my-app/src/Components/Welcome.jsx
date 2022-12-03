56fde70b8bc53aeca391b807cc9e57a1ea711ac8
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-02 21:10:07 -0800

    Ya tenemos el telefono puesto

--- my-app/src/Components/Welcome.jsx
+++ my-app/src/Components/Welcome.jsx
@@ -8,7 +8,7 @@ function Welcome (){
     return(
         <div className="mainContainer__welcome">
 
-            <div className="welcome-phone-container"><BsFillTelephoneFill onClick={()=>{window.open("tel:+4842566");  }} className="welcome-phone-icon"></BsFillTelephoneFill><p className="welcome-phone-text" onClick={()=>{window.open("tel:+4842566");  }}>Call Us</p></div>
+            <div className="welcome-phone-container" onClick={()=>{window.open("tel:+4842566");  }}><BsFillTelephoneFill className="welcome-phone-icon"></BsFillTelephoneFill><p className="welcome-phone-text" >Call Us</p></div>
             <Experience></Experience>
             <img className="welcome-img" src={"https://www.marsanmantenimiento.com/wp-content/uploads/2020/10/men-painting-empty-home-interior-P96RH5M-1536x1024.jpg"}></img>
            
