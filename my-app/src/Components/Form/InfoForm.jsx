7d9d31cd1effec21a34e731c898472a70ee05e91
joel gutierrez ruiz <joelgutierrezruiz11@gmail.com>
2023-02-09 19:18:48 +0100

    Hemos hecho el formulario posible para enviar

--- my-app/src/Components/Form/InfoForm.jsx
+++ my-app/src/Components/Form/InfoForm.jsx
@@ -5,26 +5,41 @@ import { BiArrowBack } from 'react-icons/bi';
 //imports
 
 function InfoForm({setNext}){
+
+    let datosCliente = {"First Name*":"","Email*":"","Phone":"","Postal Code*":"","Company Name":"","Postal Code*":"","Adress 1*":"","Adress 2*":"","City":"",city:""}
+
+    const handleChange=(e)=>{
+
+        
+
+        datosCliente[e.target.placeholder] = e.target.value
+
+        console.log(datosCliente)
+
+        
+    }
+
+
+
     return(
     <div className='mainContainer__infoForm'>
         <p className='infoForm-ul-text'>Business Contact Information:</p>
         <ul className='infoForm-list-container'>
-            <li className='infoForm-list-input name'><input placeholder='First Name*'></input><input placeholder='Last Name*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input name'><input placeholder='First Name*'></input><input placeholder='Last Name*'></input></li>
 
-            <li className='infoForm-list-input email'><input placeholder='Email*'></input></li>
-            <li className='infoForm-list-input phone'><input placeholder='Phone*'></input></li>
-            <li className='infoForm-list-input PostalCode'><input placeholder='Postal Code*'></input></li>
-            <li className='infoForm-list-input CompanyName'><input placeholder='Company Name*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input email'><input placeholder='Email*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input phone'><input placeholder='Phone*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input PostalCode'><input placeholder='Postal Code*'></input></li>
             
         </ul>
 
         <p className='infoForm-ul-text'>Property you want an estimate for:</p>
         <ul className='infoForm-list-container'>
 
-            <li className='infoForm-list-input email'><input placeholder='Adress 1*'></input></li>
-            <li className='infoForm-list-input phone'><input placeholder='Adress 2*'></input></li>
-            <li className='infoForm-list-input PostalCode'><input placeholder='City*'></input></li>
-            <li className='infoForm-list-input CompanyName'><input placeholder='Company Name*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input email'><input placeholder='Adress 1*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input phone'><input placeholder='Adress 2*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input PostalCode'><input placeholder='City*'></input></li>
+            <li onChange={handleChange} className='infoForm-list-input CompanyName'><input placeholder='Company Name*'></input></li>
             
         </ul>
 
