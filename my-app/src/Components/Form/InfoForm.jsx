7f9ad0368852ab931898b5abc86e0de198eb6930
joel gutierrez ruiz <joelgutierrezruiz11@gmail.com>
2023-02-10 22:48:19 +0100

    Formulario capta todo

--- my-app/src/Components/Form/InfoForm.jsx
+++ my-app/src/Components/Form/InfoForm.jsx
@@ -6,17 +6,20 @@ import { BiArrowBack } from 'react-icons/bi';
 
 function InfoForm({setNext}){
 
-    let datosCliente = {"First Name*":"","Email*":"","Phone":"","Postal Code*":"","Company Name":"","Postal Code*":"","Adress 1*":"","Adress 2*":"","City":"",city:""}
+    let datosCliente = {"First Name*":"","Email*":"","Phone*":"","Postal Code*":"","Company Name*":"","Adress 1*":"","Adress 2*":"","City*":""}
 
     const handleChange=(e)=>{
 
         
 
         datosCliente[e.target.placeholder] = e.target.value
-
         console.log(datosCliente)
 
         
+    }
+
+    const submitForm=()=>{
+        datosCliente["First Name*"]&&datosCliente["Email*"]&&datosCliente["Phone*"]&&datosCliente["Postal Code*"]&&datosCliente["Adress 1*"]?alert("Todo lleno"):alert("Rellena las casillas en rojo")
     }
 
 
@@ -25,21 +28,20 @@ function InfoForm({setNext}){
     <div className='mainContainer__infoForm'>
         <p className='infoForm-ul-text'>Business Contact Information:</p>
         <ul className='infoForm-list-container'>
-            <li onChange={handleChange} className='infoForm-list-input name'><input placeholder='First Name*'></input><input placeholder='Last Name*'></input></li>
-
-            <li onChange={handleChange} className='infoForm-list-input email'><input placeholder='Email*'></input></li>
-            <li onChange={handleChange} className='infoForm-list-input phone'><input placeholder='Phone*'></input></li>
-            <li onChange={handleChange} className='infoForm-list-input PostalCode'><input placeholder='Postal Code*'></input></li>
+            <li  className='infoForm-list-input name'><input onChange={handleChange} placeholder='First Name*'></input><input onChange={handleChange} placeholder='Last Name*'></input></li>
+            <li  className='infoForm-list-input email'><input onChange={handleChange} placeholder='Email*'></input></li>
+            <li  className='infoForm-list-input phone'><input onChange={handleChange} placeholder='Phone*'></input></li>
+            <li className='infoForm-list-input PostalCode'><input onChange={handleChange}  placeholder='Postal Code*'></input></li>
             
         </ul>
 
         <p className='infoForm-ul-text'>Property you want an estimate for:</p>
         <ul className='infoForm-list-container'>
 
-            <li onChange={handleChange} className='infoForm-list-input email'><input placeholder='Adress 1*'></input></li>
-            <li onChange={handleChange} className='infoForm-list-input phone'><input placeholder='Adress 2*'></input></li>
-            <li onChange={handleChange} className='infoForm-list-input PostalCode'><input placeholder='City*'></input></li>
-            <li onChange={handleChange} className='infoForm-list-input CompanyName'><input placeholder='Company Name*'></input></li>
+            <li  className='infoForm-list-input email'><input onChange={handleChange} placeholder='Adress 1*'></input></li>
+            <li  className='infoForm-list-input phone'><input onChange={handleChange} placeholder='Adress 2*'></input></li>
+            <li  className='infoForm-list-input PostalCode'><input onChange={handleChange} placeholder='City*'></input></li>
+            <li  className='infoForm-list-input CompanyName'><input onChange={handleChange} placeholder='Company Name*'></input></li>
             
         </ul>
 
@@ -47,7 +49,7 @@ function InfoForm({setNext}){
         <div className='infoForm-back'>
             <div className='infoForm-back-icon' onClick={()=>{setNext(null)}}><BiArrowBack ></BiArrowBack></div>
             
-            <button className='infoForm-stimate-button'>Get Your Stimate</button>
+            <button className='infoForm-stimate-button' onClick={submitForm}>Get Your Stimate</button>
         </div>
     </div>)
 }
